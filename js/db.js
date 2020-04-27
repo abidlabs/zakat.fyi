62a2bb48642ec4e75989638a2c56a3da0ef0f6f0
Moin Nadeem <moinnadeem@moinnadeem.com>
2020-04-27 03:40:07 -0500

    resolving comnments, cleaning up console logs

--- js/db.js
+++ js/db.js
@@ -3,28 +3,26 @@ import * as form from "./form.js";
 
 // get browser ID, IP, etc
 var info = {};
-function getBrowserData() {
-  // If your site is on CloudFlare, then you can use '/cdn-cgi/trace' instead
-  $.get('https://www.cloudflare.com/cdn-cgi/trace', function(data) {
-    data = data.split('\n');
-    $.each(data, function(i) {
-      i = i.split("=");
-      var key = i[0];
-      var value = i[1];
-      info[key] = value;
-    });
+// If your site is on CloudFlare, then you can use '/cdn-cgi/trace' instead
+$.get('https://www.cloudflare.com/cdn-cgi/trace', function(data) {
+  data = data.split('\n');
+  $.each(data, function(index, i) {
+    if (i.length==0) {
+      return;
+    }
+    var i = i.split("=");
+    var key = i[0];
+    var value = i[1];
+    info[key] = value;
   });
-  return 
-}
+});
 
 var db = firebase.firestore();
 
 export function sendToDB() {
   var finishedForm = document.getElementById("form"); 
   var form_json = form.formToJSON(finishedForm.elements);
-  console.log(form_json);
   var uid = cookies.getCookie("uid");
-  console.log("UID: " + uid);
   db.collection("web_data").doc(uid).set({
     "uid": uid,
     "browser_info": info,
