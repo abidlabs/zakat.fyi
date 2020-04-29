507a35ba76153cfa518b9608c5c9fe0a23ddccf6
Zuhayeer Musa <zuhaz3@gmail.com>
2020-04-28 21:31:07 -0700

    added email.js, centered modal

---
+++ js/email.js
@@ -0,0 +1,20 @@
+$(function() {
+	// $.ajax('https://api.mailgun.net/v3/sandboxae3003c438524f62a579d3e4d44ab002.mailgun.org/messages',
+	// {
+	// 	type:"POST",
+	// 	username: 'api',
+	// 	password: 'key-f723522f9e89dcb417d060dfc6dfb41c',
+	// 	data:{
+	// 		"html": `<h1>Test</h1>`,
+	// 		"subject": 'Your Zakat Summary',
+	// 		"from": "Zakat.fyi <salam@zakat.fyi>",
+	// 		"to": "<zuhaz3@gmail.com>"
+	// 	},
+	// 	success:function(a,b,c) {
+	// 		console.log( 'mail sent: ', b );
+	// 	}.bind(this),
+	// 	error:function( xhr, status, errText ) {
+	// 		console.log( 'mail sent failed: ', xhr.responseText );
+	// 	}
+	// });
+});
