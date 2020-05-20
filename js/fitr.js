e5bc185a616fb1a7e1aa17a21f3a03a276e94a9f
Zuhayeer Musa <zuhaz3@gmail.com>
2020-05-20 16:21:43 -0700

    added permalink for zip, allowed enter key to submit zip instead of requiring button click

--- js/fitr.js
+++ js/fitr.js
@@ -6,21 +6,54 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'UA-166050306-1');
 
-
-$.getJSON("data/fitr/zipcodes_to_closest_mosques.json", function(json) {
-    zipcodes_closest_mosques = json; // this will show the info it in firebug console
-});
-
 function getClosestMosques(zipcode){
 	// Hardcoded: TODO(Ihssan): replace this with query result
 	return zipcodes_closest_mosques[zipcode]
 }
 
+function getQueryStringMap() {
+    var QueryString = function() {
+        // This function is anonymous, is executed immediately and 
+        // the return value is assigned to QueryString!
+        var query_string = {};
+        var query = window.location.search.substring(1);
 
-$('#go-button').click(function() {
-  var zipcode = $('#input-zipcode').val()
-  var query_result = getClosestMosques(zipcode)
-   gtag('event', 'go-button-clicked')
+        if (query.charAt(query.length - 1) == '/') {
+          query = query.substr(0, query.length - 1);
+        }
+
+        var vars = query.split("&");
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split("=");
+            // If first entry with this name
+            if (typeof query_string[pair[0]] === "undefined") {
+                query_string[pair[0]] = decodeURIComponent(pair[1]);
+                // If second entry with this name
+            } else if (typeof query_string[pair[0]] === "string") {
+                var arr = [query_string[pair[0]], decodeURIComponent(pair[1])];
+                query_string[pair[0]] = arr;
+                // If third or later entry with this name
+            } else {
+                query_string[pair[0]].push(decodeURIComponent(pair[1]));
+            }
+        }
+        return query_string;
+    }();
+    return QueryString;
+}
+
+function updateUrl(zip) {
+    if (history.pushState) {
+        var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?zip=' + encodeURIComponent(zip);
+        window.history.pushState({
+            path: newurl
+        }, '', newurl);
+    }
+}
+
+function showMasjidsByZip(zipcode, recordEvent) {
+	var query_result = getClosestMosques(zipcode)
+    gtag('event', 'go-button-clicked')
 
   	if (typeof query_result !== 'undefined') {
 
@@ -48,12 +81,41 @@ $('#go-button').click(function() {
 		  $('html, body').animate({
 		      scrollTop: $("#masjid-box").offset().top
 		  }, 1000);	
-    	gtag('event', 'go-button-clicked-valid-zipcode', {'zipcode': zipcode})
+
+		if (recordEvent) {
+			updateUrl(zipcode);
+			gtag('event', 'go-button-clicked-valid-zipcode', {'zipcode': zipcode})
+		}
 
   	} else {
-  		$('.error-message').show()
-    	gtag('event', 'go-button-clicked-invalid-zipcode', {'zipcode': zipcode})
-
+  		$('.error-message').show();
+  		if (recordEvent) {
+  			gtag('event', 'go-button-clicked-invalid-zipcode', {'zipcode': zipcode})
+  		}
   	}
+}
 
-})
+$(function() {
+	$.getJSON("data/fitr/zipcodes_to_closest_mosques.json", function(json) {
+	    zipcodes_closest_mosques = json; // this will show the info it in firebug console
+
+	    // Check if zip query param exists
+	    var queryParams = getQueryStringMap();
+		if (queryParams["zip"]) {
+			$('#input-zipcode').val(queryParams["zip"]);
+			showMasjidsByZip(queryParams["zip"]);
+		}
+	});
+
+	$('#go-button').click(function() {
+		var zipcode = $('#input-zipcode').val();
+		showMasjidsByZip(zipcode, true);
+	});
+
+	$("#input-zipcode").on('keyup', function (e) {
+	    if (e.keyCode === 13) {
+	        var zipcode = $('#input-zipcode').val();
+			showMasjidsByZip(zipcode, true);
+	    }
+	});
+});
